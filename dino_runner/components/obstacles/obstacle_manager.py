87e744df832f62d3b2dba96a9a1a293ee0e004a2
MazorKing_xD <98781362+StevenRivasReyes@users.noreply.github.com>
2023-05-04 17:47:19 -0400

    CLASE 4
    empezamos a poner mas obstaculos, como el bird y el cactus

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,7 +1,8 @@
 import random
 import pygame
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import LARGE_CACTUS, SMALL_CACTUS
+from dino_runner.components.obstacles.bird import Bird
+from dino_runner.utils.constants import LARGE_CACTUS, SMALL_CACTUS, BIRD
 
 
 class ObstacleManager:
@@ -10,11 +11,13 @@ class ObstacleManager:
     
     def update(self, game):
         if len(self.obstacles) == 0:
-            option_obstacles = random.randint(0,1)
-            if option_obstacles == 1:
+            option_obstacles = random.randint(0,2)
+            if option_obstacles == 0:
                 self.obstacles.append(Cactus(LARGE_CACTUS))
-            elif option_obstacles == 0:
+            elif option_obstacles == 1:
                 self.obstacles.append(Cactus(SMALL_CACTUS))
+            elif option_obstacles == 2:
+                self.obstacles.append(Bird(BIRD))
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
